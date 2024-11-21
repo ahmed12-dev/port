e51539efe3a5c43bd3885774572d5e1451012ea7
ahmed12-blockchain <ahmedibnrediwan@gmail.com>
2024-11-21 14:38:00 +0300

    filter and map understanding

--- SESSION_3/script.js
+++ SESSION_3/script.js
@@ -31,3 +31,63 @@
 
 // map is  a function That takes one Element at a time and Index
 
+const squareNum = num => num * num ;
+
+// squareNum(2)
+
+console.log(squareNum(4));
+
+const square = numbers => numbers.map(squareNum)
+
+// square([1,2,3,4,5,6])
+
+console.log(square([1,2,3,4,5,6]));
+
+const wordLength = word => word.length
+
+// wordLength("ahmed")
+
+console.log(wordLength("ahmed"))
+
+const getLength = words => words.map(wordLength)
+
+getLength(["ahmed", "is", "good"])
+
+console.log(getLength(["ahmed", "is", "good"]))
+
+
+
+const thisIsAnArrayOfObject = [{name:"Ahmed"}, {name:"Bilal"},{name:"Umi"}]
+
+const extractNameFromObj = obj => obj.name
+
+extractNameFromObj ({name:"ahmed"})
+
+x = thisIsAnArrayOfObject.map(extractNameFromObj)
+
+console.log(x)
+
+
+// Fillter
+
+const numbers = [1,3,5,2,22,11,9]
+
+const isOdd = num => num % 2 != 0
+
+y = isOdd(3)
+
+console.log(y)
+
+//  to use filter, the callback function should return boolean 
+
+const oddNumberOnly = numbers.filter(isOdd)
+
+z = oddNumberOnly
+
+console.log(z)
+
+const onlyBooleans = numbers.map(isOdd)
+
+a = onlyBooleans
+
+console.log(a)
